8fce89413a6f0d08851de0087805c9c6545ab7f4
maziar <sojodyan@gmail.com>
2024-11-10 15:56:50 -0500

    luhn v3

--- luhn.py
+++ luhn.py
@@ -1,11 +1,9 @@
-
-#digits="17893729974"
 def verify(digits):
     total = 0
-    for i, d in enumerate(reversed(digits[:-1])): # TODO ideally avoid slice
-        x = int(d) * (2 - i % 2)
-        total += x // 10 + x % 10
-    return (int(digits[-1]) + total ) % 10 == 0
+    for i, d in enumerate(reversed(digits)): 
+        x = int(d) * (1 + i % 2)
+        total += x // 10 + x
+    return total % 10 == 0
 
 if __name__ == '__main__':
     assert verify("17893729974")
