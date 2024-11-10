725c2ad1c617c79b2c912f14fa0440c11a914a37
maziar <sojodyan@gmail.com>
2024-11-10 15:00:59 -0500

    add enumrate, multiplier

--- luhn.py
+++ luhn.py
@@ -1,8 +1,14 @@
 
 #digits="17893729974"
 def verify(digits):
-    for d in reversed(digits[:-1]):
-        print(d)
+    for i, d in enumerate(reversed(digits[:-1])): # TODO ideally avoid slice
+        if i % 2 == 0:
+            multiplier = 2
+        else:
+            multiplier = 1
+        x = int(d) * multiplier # TODO name x
+        xx = x // 10 + x % 10
+        print(xx)
     return False
 
 if __name__ == '__main__':
